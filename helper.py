33e9bcbf3d94962438a93024fa23f6531487bc18
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-04-12 08:13:42 -0400

    helper script to auto-rearchive build.zip

--- helper.py
+++ helper.py
@@ -5,6 +5,7 @@ import os
 import argparse
 import time
 from corelib import dynamodb, utility, s3, static_resources
+from zipfile import ZipFile
 
 
 def wait_for_countdown(seconds):
@@ -16,14 +17,82 @@ def wait_for_countdown(seconds):
     print('    Done')
 
 
+ARK_ARCHIVER_LAMBDA = 'arc-archiver-lambda'
+
+def update_lambda():
+    build_path = 'build'
+    build_src_path = os.path.join(build_path, 'src')
+    
+    build_base_zip_file = 'build_base.zip'
+    build_zip_file = 'build.zip'
+    archivelib_dir = 'archivelib'
+    corelib_dir = 'corelib'
+    lambda_function_file = 'lambda_function_archiver.py'
+    cache_dir = '__pycache__'
+
+    dst_archivelib_path = os.path.join(build_src_path, archivelib_dir)
+    dst_corelib_path = os.path.join(build_src_path, corelib_dir)
+    dst_archivelib_cache_path = os.path.join(os.path.join(build_src_path, archivelib_dir), cache_dir)
+    dst_corelib_cache_path = os.path.join(os.path.join(build_src_path, corelib_dir), cache_dir)
+
+    if not os.path.exists(build_base_zip_file):
+        print('Error: No', build_base_zip_file, 'file found!')
+        return
+
+    print('Creating temporary dir', build_path)
+    if os.path.exists(build_path):
+        shutil.rmtree(build_path)
+    os.makedirs(build_path)
+
+    print('Deleting', build_zip_file)
+    if os.path.exists(build_zip_file):
+        os.remove(build_zip_file)
+
+    print('Extracting all files in ' + build_base_zip_file + ' to ' + build_path)
+    with ZipFile(build_base_zip_file, 'r') as zipObj:
+        zipObj.extractall(build_path)
+
+    print('Deleting all files in ' + build_src_path)
+    if os.path.exists(build_src_path):
+        shutil.rmtree(build_src_path)
+    os.makedirs(build_src_path)
+
+    print('Copying ' + archivelib_dir + ' to ' + build_src_path)
+    shutil.copytree(archivelib_dir, dst_archivelib_path)
+    print('Copying ' + corelib_dir + ' to ' + build_src_path)
+    shutil.copytree(corelib_dir, dst_corelib_path)
+    print('Copying ' + lambda_function_file + ' to ' + build_src_path)
+    shutil.copy2(lambda_function_file, os.path.join(build_src_path, lambda_function_file))
+
+    print('Deleting', dst_archivelib_cache_path)
+    if os.path.exists(dst_archivelib_cache_path):
+        shutil.rmtree(dst_archivelib_cache_path)
+
+    print('Deleting', dst_corelib_cache_path)
+    if os.path.exists(dst_corelib_cache_path):
+        shutil.rmtree(dst_corelib_cache_path)
+
+    print('Zipping all files in', build_path, 'to', build_zip_file)
+    shutil.make_archive(build_path, 'zip', build_path)
+    
+    print('Deleting temporary dir', build_path)
+    shutil.rmtree(build_path)
+
+    print('    Succeeded')
+    print('\n')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--update_resources', help='Update all static resources to S3', action='store_true')
     parser.add_argument('--clear_archive', help='Clear all Archive Data (DynamoDB + S3)', action='store_true')
     parser.add_argument('--clear_all', help='Clear all Account and Archive Data (DynamoDB + S3)', action='store_true')
+    parser.add_argument('--update_lambda', help='Update Lambda Function(' + ARK_ARCHIVER_LAMBDA + ')', action='store_true')
+
     parser.add_argument('--account_table', help='Print ' + dynamodb.ACCOUNT_TABLE + ' from DynamoDB‎', action='store_true')
     parser.add_argument('--archive_table', help='Print ' + dynamodb.ARCHIVE_TABLE + ' from DynamoDB', action='store_true')
     parser.add_argument('--s3', help='Print ' + s3.BUCKET + ' from S3', action ='store_true')
+
     parser.add_argument('--reset', help='Reset everything', action='store_true')
     parser.add_argument('--confirm', help='Confirm', action='store_true')
 
@@ -68,6 +137,10 @@ if __name__ == '__main__':
         print('    Succeeded')
 
         print('\n')
+
+
+    if args.update_lambda:
+        update_lambda()
 
 
     if args.account_table:
