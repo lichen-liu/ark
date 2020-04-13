31295066d06a1612e2f529e8590a07564a8dd20e
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-04-12 21:11:43 -0400

    updated archive_url logic

--- archivelib/archive_lambda.py
+++ archivelib/archive_lambda.py
@@ -12,48 +12,56 @@ def archive_url(original_url, username, running_locally):
     '''
     error_message = None
     
-    found = dynamodb.pop_account_archive_request_by(
-        list_name=dynamodb.ACCOUNT_TABLE_ARCHIVE_PENDING_REQUEST_LIST, username=username, original_url=original_url)
+    print('adjust_url(' + original_url + ')')
+    url = url_util.adjust_url(original_url)
+    if url is not None:
+        # Record the current datetime
+        utc_datetime = datetime.datetime.utcnow()
+        utc_datetime_str = str(utc_datetime)
 
-    if found:
-        print('adjust_url(' + original_url + ')')
-        url = url_util.adjust_url(original_url)
-        if url is not None:
-            # Register the url and date to arkArchive
-            utc_datetime = datetime.datetime.utcnow()
-            utc_datetime_str = str(utc_datetime)
+        # Save it on archive website
+        try:
+            print('archiveis.capture(' + url + ')')
+            initial_archive_md_url = archiveis.capture(url)
+        except Exception as e:
+            print('Unexpected exception: ' + str(e))
+            initial_archive_md_url = None
+        
+        # Screenshot the url webpage
+        print('take_url_webpage_snapshot(' + url + ')')
+        url_webpage_png, _url_inner_html = webpage_snapshot.take_url_webpage_snapshot(url=url, running_locally=running_locally)
 
-            # Save it on archive website
-            try:
-                print('archiveis.capture(' + url + ')')
-                initial_archive_md_url = archiveis.capture(url)
-            except Exception as e:
-                print('Unexpected exception: ' + str(e))
-                initial_archive_md_url = None
+        # Pop from pending
+        dynamodb.pop_account_archive_request_by(list_name=dynamodb.ACCOUNT_TABLE_ARCHIVE_PENDING_REQUEST_LIST,
+            username=username, original_url=original_url)
 
-            is_newly_created = dynamodb.create_new_archive(url=url, datetime=utc_datetime_str, username=username, archive_md_url=initial_archive_md_url)
-            if is_newly_created:
-                # Screenshot the url webpage
-                print('take_url_webpage_snapshot(' + url + ')')
-                url_webpage_png, _url_inner_html = webpage_snapshot.take_url_webpage_snapshot(url=url, running_locally=running_locally)
+        # Create new archive entry on DynamoDB
+        dynamodb.create_new_archive(url=url, datetime=utc_datetime_str, username=username, archive_md_url=initial_archive_md_url)
 
-                # Store the screenshot on S3
-                archive_id, _, _ = dynamodb.get_archive_info(url=url, datetime=utc_datetime_str)
-                url_webpage_png_s3_key = s3.WEBPAGE_SCREENSHOT_DIR + archive_id + '.png'
-                s3.upload_file_bytes_object(key=url_webpage_png_s3_key, file_bytes=url_webpage_png)
+        # Store the screenshot on S3
+        archive_id, _, _ = dynamodb.get_archive_info(url=url, datetime=utc_datetime_str)
+        url_webpage_png_s3_key = s3.WEBPAGE_SCREENSHOT_DIR + archive_id + '.png'
+        s3.upload_file_bytes_object(key=url_webpage_png_s3_key, file_bytes=url_webpage_png)
 
-                # Store the text of the webpage on S3
-                # url_webpage_text = clean_text(extract_text(url_inner_html)).encode()
-                # url_weboage_text_s3_key = s3.WEBPAGE_TEXT_DIR + archive_id + '.txt'
-                #s3.upload_file_bytes_object(key=url_weboage_text_s3_key, file_bytes=url_webpage_text)
-            else:
-                error_message = 'Error: The archive was already created for url(' + url + ') on (' + utc_datetime_str + ')!'
-        else:
-            dynamodb.push_account_archive_request(list_name=dynamodb.ACCOUNT_TABLE_ARCHIVE_FAILED_REQUEST_LIST,
-                                                username=username, original_url=original_url)
-            error_message = 'Invalid URL: ' + original_url
+        # Store the text of the webpage on S3
+        # url_webpage_text = clean_text(extract_text(url_inner_html)).encode()
+        # url_weboage_text_s3_key = s3.WEBPAGE_TEXT_DIR + archive_id + '.txt'
+        #s3.upload_file_bytes_object(key=url_weboage_text_s3_key, file_bytes=url_webpage_text)
+
+        # Early-exit for success
+        return None
     else:
-        error_message = 'URL ' + original_url + ' archiving request is already processed!'
+        error_message = 'Invalid URL: ' + original_url
+
+    # All success must early-exit
+    assert error_message
+
+    # Pop from pending
+    dynamodb.pop_account_archive_request_by(list_name=dynamodb.ACCOUNT_TABLE_ARCHIVE_PENDING_REQUEST_LIST,
+        username=username, original_url=original_url)
+    # Add into failed
+    dynamodb.push_account_archive_request(list_name=dynamodb.ACCOUNT_TABLE_ARCHIVE_FAILED_REQUEST_LIST,
+        username=username, original_url=original_url)
 
     return error_message
 
